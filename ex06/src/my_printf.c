486fe5526b97779952504cd31710da3320e19a08
MaRk0pOl0kEk <anderhil1998@gmail.com>
2018-06-15 11:01:35 +0300

    ex06 Test success

--- ex06/src/my_printf.c
+++ ex06/src/my_printf.c
@@ -1,83 +1,124 @@
 #include <stdarg.h>
+
+#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "itoa.h"
+#include "strcpy.h"
 
 typedef int bool;
 #define true 1
 #define false 0
 #define EOF (-1)
 
-int my_strlen(const char *str){
-  int i = 0;
-  for (i = 0; str[i] != '\0'; ++i){
-}
-return i;
+int my_printf(const char *format, ...);
+
+int padding(const char *format, int *curr_pos )
+{
+   int res = 0;
+   while( format[ *curr_pos] >= '0' && format[ *curr_pos] <= '9')
+   {
+      res = res * 10 + (int)format[*curr_pos] - (int)'0';
+      ++(*curr_pos);
+   }
+   return res;
 }
 
-int check_flag(char flag){
-  int type_name;
-  if ( flag == 's' )      type_name = 1;
-  else if ( flag == 'd' ) type_name = 2;
-  else                    type_name = 0;
-  return type_name;
+void char_out( char *arg, int padding_)
+{
+
+                int arg_size = 0;
+                for (int i = 0 ; *(arg + i) != '\0'; ++i)
+                {
+                  arg_size++;
+                }
+                //printf("%d", padding_);
+                if( arg_size > padding_) padding_ = arg_size;
+                char * out = malloc( (padding_ - arg_size) * sizeof(char *));
+                memset( out, ' ', sizeof(char *) * (padding_ - arg_size) );
+                write( 1, out, padding_ - arg_size);
+                write( 1, arg, arg_size );
+}
+
+void int_out( int arg, int padding_, const char ch)
+{
+                int arg_size = my_strlen( my_itoa( arg));
+
+                if (arg_size > padding_)
+                padding_ = arg_size;
+
+                char arr[padding_];
+
+                memset( arr, ch, padding_ - arg_size);
+                strcpy(arr + padding_ - arg_size, my_itoa( arg));
+                write ( 1, arr, padding_);
 }
 
 int my_printf(const char *format, ...)
 {
-  int arg_count = 100, format_Size = my_strlen(format) - 1;  // костиль, нада fix
-  va_list arg_;
-
-  va_start( arg_, arg_count );
-  for(int i = 0; i < format_Size; ++i){
-    if ( format[i] == '%' )
-    {
-      if ( !check_flag(format[i + 1]) )
+   int arg_count, format_size = strlen(format);
+   va_list arg_;
+   va_start( arg_, format);
+   bool check_null = false;
+   for(int i = 0; i < format_size; ++i)
+   {
+      if (format[i] == '%' && format[i + 1] == '0')
       {
-        if ( check_flag(format[i + 1]) == 1 )
-        {
-          char *out = malloc( strlen( va_arg( arg_, char *)));
-          out = va_arg( arg_, char *);
-          write( 1, out, sizeof(char *) * my_strlen(out));
-        }
-        if ( check_flag(format[i + 1]) == 2 )
-        {
-          double out = va_arg( arg_, double);
-          write( 1, &out, sizeof(double) );
-        }
-        else
-        {
-          if (format[i + 1] == '%')
-          {
-            write( 1, *(format + i), 1);
-          }
-          else
-          {
-            write( 1, *(format + i), 2);
-            i++;
-          }
-        }
+          check_null = true;
       }
-    }
-    else
-    {
-      write( 1, *(format + i), 1);
-    }
-  }
+      if (format[i] == '%')
+      {
+          switch( format[++i])
+          {
+              case 'd':
+              {
+                int arg = va_arg( arg_, int);
+                int_out( arg, 0, ' ');
+                break;
+              }
+
+              case 's':
+              {
+                char * arg = va_arg(  arg_, char *);
+                char_out ( arg, 0);
+                check_null = false;
+                break;
+              }
+
+              case '%':
+              {
+                write( 1, format + i, 1);
+                break;
+              }
+              default:
+              {
+                int padding_ = padding(format, &i);
+                if(format[i] == 's')
+                {
+                  char *arg = va_arg( arg_, char *);
+                  char_out(arg, padding_);
+                }
+
+                else if(format[i] == 'd')
+                {
+                  int arg = va_arg( arg_, int);
+                  if( check_null)
+                    int_out(arg, padding_, '0');
+                  else
+                    int_out(arg, padding_, ' ');
+                }
+                check_null = false;
+                break;
+              }
+          }
+      }
+      else
+      {
+        write(1, format + i, 1);
+      }
+   }
+
   va_end( arg_ );
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
