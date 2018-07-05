f556fef2204b4fc546e1ff92b44cd72fed76aadb
MaRk0pOl0kEk <anderhil1998@gmail.com>
2018-07-05 16:04:21 +0300

    worked malloc

--- ex12/src/halloc.c
+++ ex12/src/halloc.c
@@ -1,31 +1,35 @@
 #include <stddef.h>
 #include "alloc_block.h"
 #include <unistd.h>
+#include <stdio.h>
 
-alloc_b *head = NULL;
+ alloc_b *head = NULL;
 
-void block_init(alloc_b *block, size_t size)
+void block_init(alloc_b **block, size_t size)
 {
-  block->size = size;
-  block->next = NULL;
-  block->free = 0;
+  (*block)->size = size;
+  (*block)->next = NULL;
+  (*block)->free = 0;
 }
 
-alloc_b *create_block( alloc_b *new, size_t size)
+alloc_b *create_block( alloc_b *new, alloc_b *last, size_t size)
 {
-  new = sbrk(sizeof(alloc_b *) + size);
-  if (new == (void *) - 1)
+  new = sbrk(0);
+  alloc_b *ptr = sbrk(sizeof(alloc_b) + size);
+  if (ptr == (void *) - 1)
     return NULL;
-  block_init(new, size);
+  block_init(&new, size);
+  if (last != NULL)
+    last->next = new;
   return new;
 }
 
-alloc_b *reuse_block(alloc_b **last, size_t size)
+alloc_b *reuse_block(alloc_b *last, size_t size)
 {
   alloc_b *iterator = head;
-  while (!(iterator->free && iterator->size >= size))
+  while (iterator != NULL && !(iterator->free && iterator->size >= size))
   {
-    *last = iterator;
+    last = iterator;
     iterator = iterator->next;
   }
   return iterator;
@@ -34,28 +38,33 @@ alloc_b *reuse_block(alloc_b **last, size_t size)
 void free(alloc_b *ptr)
 {
   if (ptr == NULL)
+  {
     return;
+  }
   (ptr - 1)->free = 1;
 }
 
 void *halloc(size_t size)
 {
+  if (size == 0)
+    return NULL;
+
   alloc_b *new;
   if (head == NULL)
   {
-    create_block(head, size);
-    return head;
+    head = create_block(head, NULL, size);
+    return head + 1;
   }
-
   else
   {
     alloc_b *last;
 
     last = head;
-    new = reuse_block(&last, size);
+    new = reuse_block(last, size);
+
     if(new == NULL)
     {
-      new = create_block(new, size);
+      new = create_block(new, last, size);
       if (new == NULL)
         return NULL;
     }
@@ -64,7 +73,6 @@ void *halloc(size_t size)
   }
   return new + 1;
 }
-
 
 
 
